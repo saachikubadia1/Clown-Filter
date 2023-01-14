5eed4069e88e2669efebd89f7439e2d59c4ec68d
saachikubadia <93033395+saachikubadia@users.noreply.github.com>
2023-01-14 19:29:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
     video=createCapture(VIDEO);
+    video.size(300, 300);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -17,7 +18,7 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 300, 300);
-    image(clown_nose, nosex, nosey, 30, 30);
+    image(clown_nose, nosex-15, nosey-15, 30, 30);
     
 }
 
